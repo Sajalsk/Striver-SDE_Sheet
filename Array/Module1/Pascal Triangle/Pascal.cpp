a2ca365eabd58a021317c27a69bea8a701b2363f
sajal <sajal.khandelwal@xcelore.com>
2025-02-10 22:30:23 +0530

    update-array

--- Array/Module1/Pascal Triangle/Pascal.cpp
+++ Array/Module1/Pascal Triangle/Pascal.cpp
@@ -11,7 +11,7 @@ public:
         for (int i=0;i<numRows;i++) {
 
             res[i].resize(i+1);
-            res[i][0]=res[i][i]=1;
+            res[i][0]=res[i][i]=1;                   // keeping the first and last index as always 1
             
             for (int j=1;j<i;j++) {                  // <i !important
                 res[i][j]=res[i-1][j-1]+res[i-1][j];
