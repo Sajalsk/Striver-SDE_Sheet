411cabfa79466decf67678e58c19db9d81f8e4e6
sajal <sajal.khandelwal@xcelore.com>
2025-02-19 01:45:05 +0530

    optimised_array

--- Array/Module1/Stock Buy & Sell/Buy.cpp
+++ Array/Module1/Stock Buy & Sell/Buy.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 class Solution {
-public:
-
-    int maxProfit(vector<int>& prices) {
-        
-        int max1=INT_MIN;
-
-       for(int i=1;i<prices.size();i++) {
-
-          int min1=min(min1,prices[i]);              // keeping track of minimum in the array.
-          int diff=prices[i]-min1;
-           
-          max1=max(max1,diff);
-       }
-        return max1;
-    }
-};
+    public:
+        int maxProfit(vector<int>& prices) {
+            int n = prices.size();
+            if (n == 0) return 0;  // Edge case: empty prices array
+    
+            int min_val = INT_MAX;
+            int max_profit = 0;  // Initialize to 0 because no transaction means no profit
+    
+            for (int i = 0; i < n; i++) {
+                min_val = min(min_val, prices[i]);  // Update min price seen so far
+                int diff = prices[i] - min_val;     // Calculate profit
+                max_profit = max(max_profit, diff); // Update max profit
+            }
+            return max_profit;
+        }
+    };
+    
--- Array/Module2/MergeInterval/merge.cpp
+++ Array/Module2/MergeInterval/merge.cpp
@@ -12,7 +12,7 @@ public:
     vector<vector<int>>res{intervals[0]};        // res={i,j};
 
     for (int i = 1; i < intervals.size(); i++) {
-        if (res.back()[1] < intervals[i][0]) {    //interval is grtr push it
+        if (intervals[i][0] > res.back()[1] ) {    //interval is grtr push it
             res.push_back(intervals[i]);     // the last colmn chnged to grtr
         } else {
             res.back()[1] = max(res.back()[1], intervals[i][1]);   //overlap, update end
