6b950a976f5360fa4b7157c3044ad2f45ab4711a
sajal <sajal.khandelwal@xcelore.com>
2025-04-01 22:51:09 +0530

    balanced-paranthesis

--- Stack/Module1/BalancedParanthesis.cpp
+++ Stack/Module1/BalancedParanthesis.cpp
@@ -2,34 +2,35 @@
 using namespace std;
 
 class Solution {
-public:
-
-    bool isValid(string s) {
-        
-        stack<int>st;
-
-        if(s.length()%2!=0) return false;
-        
-        for(int i=0;i<s.length();i++) {
+    public:
+        bool isValid(string s) {
             
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
-
-            else if(s[i]==')')  if(st.top()!='(') return false;
-            else st.pop();
+            if(s.length()%2!=0) return false;
+            stack<char>st;
             
-            else if(s[i]=='}')  if(st.top()!='{') return false;
-            else st.pop();
+            for(int i=0;i<s.length();i++) {
+                
+                if(s[i]=='(' || s[i]=='{' || s[i]=='[') {
+                    st.push(s[i]);   
+                } 
+                   else if (s[i]==')') {
+                     if(st.empty() || st.top()!='(' ) return false;
+                     else st.pop();
+                } 
+                  else if (s[i]=='}') {
+                     if(st.empty() || st.top()!='{') return false;
+                     else st.pop();
+                } 
+                  else if (s[i]==']') {
+                     if(st.empty() || st.top()!='[') return false;
+                     else st.pop();
+                }
+            }
             
-            else if(s[i]==']')  if(st.top()!='[') return false;
-            else st.pop();
-            
-            else return false;
+            if(!st.empty()) return false;
+            return true;
         }
-        
-        if(!st.empty()) return false;
-        return true;
-    }
-};
+    };
 
 // "{[]}"
 
--- Tree/Striver/Module2/Diameter.cpp
+++ Tree/Striver/Module2/Diameter.cpp
@@ -1,39 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-template <typename T>
-class TreeNode {
+class Solution {
     public:
-    
-    T data;
-    vector <TreeNode*> children;
-    
-    TreeNode(T data) {
-        this->data=data;
-    }
-
-class Solution { 
-public:
-    
-     int ans=0,curr;
-     int height(TreeNode* root) {
-         
-        if(!root) return 0;
-        int lh=0,rh=0;
+        int ans = 0;
         
-        lh+=Solve(root->left);
-        rh+=Solve(root->right);
-         
-        curr=lh+rh;
-        ans=max(ans,curr);
-        
-        return 1+max(lh,rh);
-    }
+        int Solve(TreeNode* root) {
+            if (!root) return 0;
+            
+            int lh = Solve(root->left);
+            int rh = Solve(root->right);
+            
+            ans = max(ans, lh + rh); 
+            
+            return 1 + max(lh, rh);  
+        }
     
-    int diameterOfBinaryTree(TreeNode* root) {
-        
-        height(root);
-        return ans;
-    }
-};
-};
+        int diameterOfBinaryTree(TreeNode* root) {
+            Solve(root);
+            return ans;
+        }
+    };
+    
