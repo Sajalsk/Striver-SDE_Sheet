3a342a0dfad46743fbc5f76fe24c762065be262c
Sajal <sajal.khandelwal@xcelore.com>
2025-04-13 00:51:04 +0530

    array_module2

--- Array/Module2/MergeInterval/merge.cpp
+++ Array/Module2/MergeInterval/merge.cpp
@@ -11,7 +11,7 @@ public:
 
     vector<vector<int>>res{intervals[0]};        // res={i,j};
 
-    for (int i = 1; i < intervals.size(); i++) {
+    for (int i = 0; i < intervals.size(); i++) {   // i=0 or 1 --> time complexity changes.
         if (intervals[i][0] > res.back()[1] ) {    //interval is grtr push it
             res.push_back(intervals[i]);     // the last colmn chnged to grtr
         } else {
